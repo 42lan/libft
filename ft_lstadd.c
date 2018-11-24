103a71de958f445350387b2fc4c0cdbb69ff30ed
4slan <amalsago@student.42.fr>
2018-11-24 11:20:20 +0100

    mistake in third operand: new instead *new

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 18:06:38 by amalsago          #+#    #+#             */
-/*   Updated: 2018/11/24 10:43:42 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/11/24 11:19:15 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd(t_list **alst, t_list *new)
 {
-	if (alst && *alst && *new)
+	if (alst && *alst && new)
 	{
 		new->next = (*alst);
 		(*alst) = new;
