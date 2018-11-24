9157d217491015a4259394c3987ec7b9cb84490f
4slan <amalsago@student.42.fr>
2018-11-24 10:49:43 +0100

    checking if passed argument are valid

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 17:50:47 by amalsago          #+#    #+#             */
-/*   Updated: 2018/11/21 18:05:58 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/11/24 10:49:14 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	while (*alst)
+	if (alst && *alst && del)
 	{
-		del((*alst)->content, (*alst)->content_size);
-		free(*alst);
-		(*alst) = (*alst)->next;
+		while (*alst)
+		{
+			del((*alst)->content, (*alst)->content_size);
+			free(*alst);
+			(*alst) = (*alst)->next;
+		}
 	}
 }
