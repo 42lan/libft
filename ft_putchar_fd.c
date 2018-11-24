7d2bfd6234a483ad36ffd48bd955627618c84fd3
4slan <amalsago@student.42.fr>
2018-11-24 11:43:28 +0100

    without conditions

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 08:10:34 by amalsago          #+#    #+#             */
-/*   Updated: 2018/11/24 10:56:44 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/11/19 08:24:20 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,5 @@
 
 void	ft_putchar_fd(char c, int fd)
 {
-	if (c && fd)
-		write(fd, &c, 1);
+	write(fd, &c, 1);
 }
