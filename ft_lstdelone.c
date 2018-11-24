f2f91458debe21c69d591299423c9733e02c121d
4slan <amalsago@student.42.fr>
2018-11-24 10:50:56 +0100

    checking if passed argument are valid

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 16:59:25 by amalsago          #+#    #+#             */
-/*   Updated: 2018/11/21 17:06:09 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/11/24 10:50:44 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	del((*alst)->content, (*alst)->content_size);
-	ft_memdel((void **)alst);
+	if (alst && *alst && del)
+	{
+		del((*alst)->content, (*alst)->content_size);
+		ft_memdel((void **)alst);
+	}
 }
