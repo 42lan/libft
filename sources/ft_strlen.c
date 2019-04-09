e41a1999648d415d87417b8144b1e82a81807e0b
4slan <amalsago@student.42.fr>
2019-04-09 11:30:04 +0200

    checking if s1 is empty

--- sources/ft_strlen.c
+++ sources/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 20:12:12 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 16:58:01 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/09 11:29:42 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t		ft_strlen(const char *s1)
 	int		len;
 
 	len = 0;
+	if (!s1)
+		return (0);
 	while (*s1++)
 		len++;
 	return (len);
