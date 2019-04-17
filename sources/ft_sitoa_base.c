46803368d560ce71800a7f9597ff6ab290d5ad70
4slan <amalsago@student.42.fr>
2019-04-17 19:14:48 +0200

    creating ft_sitoa_base.c and ft_uitoa_base.c

--- sources/ft_sitoa_base.c
+++ sources/ft_sitoa_base.c
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*   ft_sitoa_base.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 00:39:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/17 00:46:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/17 17:26:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "libft.h"
+# define BASE "0123456789abcdefghijklmnopqrstuvwxyz"
 
-static size_t	ft_silen(intmax_t number, int base)
+static size_t	ft_silen(intmax_t number, unsigned base)
 {
 	size_t		length;
 
 	length = 0;
 	if (number == 0)
 		return (1);
-	if (number < 0)
+	else if (number < 0)
 		length = 1;
 	while (number != 0)
 	{
@@ -29,10 +31,26 @@ static size_t	ft_silen(intmax_t number, int base)
 	return (length);
 }
 
-char			*ft_sitoa_base(intmax_t number)
+char			*ft_sitoa_base(intmax_t number, unsigned base)
 {
-	char		str[];
+	char		*str;
 	size_t		length;
 
-	length = ft_silen(number);
+	if (base < 2 || base > 36)
+		exit (0);
+	length = ft_silen(number, base);
+	if (!(str = ft_strnew(length)))
+		return (NULL);
+	if (number < 0)
+	{
+		--length;
+		str[0] = '-';
+		number *= -1;
+	}
+	while (length != 0)
+	{
+		str[length--] = BASE[number % base];
+		number /= base;
+	}
+	return (str);
 }
