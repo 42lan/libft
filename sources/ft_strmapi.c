4f89e01331831f7242955e1b8307e04e7552ca5f
4slan <amalsago@student.42.fr>
2019-04-29 19:12:49 +0200

    removing i and placing it in while()

--- sources/ft_strmapi.c
+++ sources/ft_strmapi.c
@@ -6,26 +6,23 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 10:40:43 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 15:48:59 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/29 10:26:23 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char				*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	unsigned int	i;
-	char			*str_new;
+	int		i;
+	char	*str_new;
 
 	if (!s || !f)
 		return (NULL);
 	if (!(str_new = ft_strnew(ft_strlen(s))))
 		return (NULL);
-	i = 0;
-	while (s[i] != '\0')
-	{
+	i = -1;
+	while (s[++i] != '\0')
 		str_new[i] = f(i, s[i]);
-		i++;
-	}
 	return (str_new);
 }
