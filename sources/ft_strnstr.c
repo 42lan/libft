1211e5289f1707c9207cc92329e5be5ff6791905
4slan <amalsago@student.42.fr>
2019-04-29 19:14:27 +0200

    normalization; placing && operator in new line

--- sources/ft_strnstr.c
+++ sources/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 16:19:35 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 15:40:50 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/29 17:52:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char		*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		if (haystack[i] == needle[j])
 		{
 			while (haystack[i + j] == needle[j] && needle[j] != '\0'
-			       && i + j < len)
+					&& i + j < len)
 				j++;
 			if (needle[j] == '\0')
 				return ((char *)haystack + i);
