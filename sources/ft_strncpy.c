f983ef59c97d4b71c638cf47e457029efc7c765a
4slan <amalsago@student.42.fr>
2019-04-29 19:14:04 +0200

    placing ++i and i++ in src[] and dst[]

--- sources/ft_strncpy.c
+++ sources/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/10 11:43:55 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 15:40:34 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/29 10:30:07 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,10 @@ char		*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	size_t	i;
 
-	i = 0;
-	while (src[i] != '\0' && i < len)
-	{
+	i = -1;
+	while (src[++i] != '\0' && i < len)
 		dst[i] = src[i];
-		i++;
-	}
 	while (i < len)
-	{
-		dst[i] = '\0';
-		i++;
-	}
+		dst[i++] = '\0';
 	return (dst);
 }
