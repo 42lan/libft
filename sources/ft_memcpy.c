6752b157db0602307750085ccb0679c00590fe7a
4slan <amalsago@student.42.fr>
2019-04-29 19:00:19 +0200

    removing i++ inside while

--- sources/ft_memcpy.c
+++ sources/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/11 15:53:20 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 15:45:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/29 10:07:27 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,8 @@ void		*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
-	i = 0;
-	while (i < n)
-	{
+	i = -1;
+	while (++i < n)
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-		i++;
-	}
 	return (dst);
 }
