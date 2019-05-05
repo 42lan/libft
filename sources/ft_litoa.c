9db21920fdfcc2095dd4ddcf604db8d0471f1453
4slan <amalsago@student.42.fr>
2019-05-05 19:13:38 +0200

    removing ft_lnblen() and using ft_silen() instead

--- sources/ft_litoa.c
+++ sources/ft_litoa.c
@@ -5,36 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/10 18:39:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/01 14:12:33 by amalsago         ###   ########.fr       */
+/*   Created: 2019/05/05 19:00:58 by amalsago          #+#    #+#             */
+/*   Updated: 2019/05/05 19:02:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t	ft_lnblen(intmax_t number)
-{
-	size_t		length;
-
-	length = 0;
-	if (number == 0)
-		return (1);
-	if (number < 0)
-		length++;
-	while (number != 0)
-	{
-		number /= 10;
-		length++;
-	}
-	return (length);
-}
 
 char			*ft_litoa(intmax_t number)
 {
 	size_t		len;
 	char		*str;
 
-	len = ft_lnblen(number);
+	len = ft_silen(number);
 	if (number <= LLONG_MIN)
 	{
 		str = ft_strdup("-9223372036854775808");
