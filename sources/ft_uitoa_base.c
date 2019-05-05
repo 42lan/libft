e57fcb0da4470be481176208aadd94b4eb402607
4slan <amalsago@student.42.fr>
2019-05-05 17:10:00 +0200

    adding check if number is equal to 0

--- sources/ft_uitoa_base.c
+++ sources/ft_uitoa_base.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 17:39:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/05 12:41:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/05 17:09:30 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,9 @@ char			*ft_uitoa_base(uintmax_t number, int base, int uppercase)
 	length = ft_uilen(number, base);
 	if (!(str = ft_strnew(length)))
 		return (NULL);
-	if (uppercase == 1)
+	if (number == 0)
+		str[0] = '0';
+	else if (uppercase == 1)
 		while (number > 0)
 		{
 			str[--length] = BASE_UPPER[number % base];
