fcfecdb08e938b924be29323cf82de5fa9c4e652
4slan <amalsago@student.42.fr>
2019-05-05 16:23:24 +0200

    removing static ft_uilen(); refactoring body of ft_uitoa_base()

--- sources/ft_uitoa_base.c
+++ sources/ft_uitoa_base.c
@@ -6,26 +6,11 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 17:39:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/29 19:19:40 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/05 12:41:08 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t	ft_uilen(uintmax_t number, int base)
-{
-	size_t		length;
-
-	length = 0;
-	if (number == 0)
-		return (1);
-	while (number != 0)
-	{
-		number /= base;
-		++length;
-	}
-	return (length);
-}
 
 char			*ft_uitoa_base(uintmax_t number, int base, int uppercase)
 {
@@ -33,15 +18,21 @@ char			*ft_uitoa_base(uintmax_t number, int base, int uppercase)
 	size_t		length;
 
 	if (base < 2 || base > 36)
-		exit(0);
+		return (NULL);
 	length = ft_uilen(number, base);
 	if (!(str = ft_strnew(length)))
 		return (NULL);
-	while (length != 0)
-	{
-		str[--length] = (uppercase == 0)
-						? BASE_LOWER[number % base] : BASE_UPPER[number % base];
-		number /= base;
-	}
+	if (uppercase == 1)
+		while (number > 0)
+		{
+			str[--length] = BASE_UPPER[number % base];
+			number /= base;
+		}
+	else
+		while (number > 0)
+		{
+			str[--length] = BASE_LOWER[number % base];
+			number /= base;
+		}
 	return (str);
 }
