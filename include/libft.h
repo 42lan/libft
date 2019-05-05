f03a456e5af739b0e3b89c51065d3dfef5969255
4slan <amalsago@student.42.fr>
2019-05-05 19:13:14 +0200

    adding prototype of ft_itoa_static()

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 12:05:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/05 12:45:42 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/05 19:12:59 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,5 +102,6 @@ size_t				ft_silen(intmax_t number, int base);
 char				*ft_uitoa_base(uintmax_t number, int base, int uppercase);
 size_t				ft_uilen(uintmax_t number, int base);
 char				*ft_litoa(intmax_t number);
+char				*ft_itoa_static(intmax_t n);
 
 #endif
