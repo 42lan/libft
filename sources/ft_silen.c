bbfd6a18d944a9bc7a1437a48d0f89927ce0dd70
4slan <amalsago@student.42.fr>
2019-05-05 16:20:43 +0200

    definition of ft_silen.c

---
+++ sources/ft_silen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_silen.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/05 12:43:10 by amalsago          #+#    #+#             */
+/*   Updated: 2019/05/05 12:43:34 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t		ft_silen(intmax_t number, int base)
+{
+	size_t	length;
+
+	length = 0;
+	if (number == 0)
+		return (1);
+	else if (number < 0)
+		length = 1;
+	while (number != 0)
+	{
+		number /= base;
+		++length;
+	}
+	return (length);
+}
+
