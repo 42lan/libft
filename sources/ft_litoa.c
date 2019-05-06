49cf05cb5440db7d604e3a13a95bd62ecb9c2368
4slan <amalsago@student.42.fr>
2019-05-06 15:54:46 +0200

    refactoring

--- sources/ft_litoa.c
+++ sources/ft_litoa.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/05 19:00:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/06 08:59:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/06 15:50:06 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 char			*ft_litoa(intmax_t number)
 {
-	size_t		len;
 	char		*str;
+	size_t		length;
 
-	len = ft_silen(number, 10);
 	if (number <= LLONG_MIN)
 	{
 		str = ft_strdup("-9223372036854775808");
 		return (str);
 	}
-	if (!(str = ft_strnew(len)))
+	length = ft_silen(number, 10);
+	if (!(str = ft_strnew(length)))
 		return (NULL);
 	if (number == 0)
-		str[len - 1] = number + '0';
+		str[0] = '0';
 	else if (number < 0)
 	{
 		str[0] = '-';
 		number *= -1;
 	}
-	while (number != 0)
+	while (number > 0)
 	{
-		str[--len] = (number % 10) + '0';
+		str[--length] = (number % 10) + '0';
 		number /= 10;
 	}
 	return (str);
