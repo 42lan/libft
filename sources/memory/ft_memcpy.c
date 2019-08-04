061b9c099b102752ae6646a902af25a465701906
4slan <amalsago@student.42.fr>
2019-08-04 17:16:59 +0200

    fixing probleme where random char is added at the end of dst

--- sources/memory/ft_memcpy.c
+++ sources/memory/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/11 15:53:20 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/29 10:07:27 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/04 16:25:36 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void		*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = -1;
-	while (++i < n)
+	while (++i <= n)
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 	return (dst);
 }
