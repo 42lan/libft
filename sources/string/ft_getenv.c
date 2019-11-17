6955fd58838fbf0984ae09a281f3ba993e7c6637
4slan <amalsago@student.42.fr>
2019-11-17 20:56:12 +0100

    refactoring: exctracting exactly matched variable

--- sources/string/ft_getenv.c
+++ sources/string/ft_getenv.c
@@ -6,29 +6,31 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/26 19:22:45 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/06 10:23:50 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/15 19:46:12 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "ft_printf.h"
 
 extern char		**environ;
 
 char			*ft_getenv(const char *name)
 {
 	int			i;
+	int			len;
 	char		*value;
 
-	i = 0;
+	i = -1;
 	value = NULL;
-	while (environ[i])
+	while (environ[++i])
 	{
-		if (ft_strnequ(environ[i], name, ft_strlen(name)))
+		len = ft_strlen(name);
+		if (ft_strnequ(environ[i], name, len) && environ[i][len] == '=')
 		{
 			value = ft_strchr(environ[i], '=') + 1;
 			break ;
 		}
-		i++;
 	}
 	return (value);
 }
