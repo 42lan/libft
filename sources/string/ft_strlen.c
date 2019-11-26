d485c9e38dc735f7cba62c4455adea2935c39188
4slan <amalsago@student.42.fr>
2019-11-26 12:14:17 +0100

    renaming because there is only one string s

--- sources/string/ft_strlen.c
+++ sources/string/ft_strlen.c
@@ -6,22 +6,22 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 20:12:12 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/20 10:48:31 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/26 12:11:10 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** Computes and returns the length of the string s1.
+** Computes and returns the length of the string s.
 */
 
-size_t		ft_strlen(const char *s1)
+size_t		ft_strlen(const char *s)
 {
 	size_t	len;
 
 	len = 0;
-	while (*s1++)
+	while (*s++)
 		len++;
 	return (len);
 }
