835a15433b637ff8c9e01513b99c8072ebbb554c
4slan <amalsago@student.42.fr>
2019-11-26 12:32:05 +0100

    definiton of ft_strarraydel()

---
+++ sources/string/ft_strarraydel.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_arraydel.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/27 11:21:28 by amalsago          #+#    #+#             */
+/*   Updated: 2019/11/26 12:31:25 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Takes as a parameter the address of the array of strings that need to be 
+** freed,  then sets its pointer to NULL.
+*/
+
+void		ft_strarraydel(char ***array_ptr)
+{
+	int		i;
+
+	i = -1;
+	if (array_ptr && *array_ptr && **array_ptr)
+	{
+		while (array_ptr[++i])
+			ft_strdel(array_ptr[i]);
+		free(*array_ptr);
+		*array_ptr = NULL;
+	}
+}
