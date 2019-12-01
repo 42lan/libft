28c9bba343081466288f7a06c70b41ee7e53ed24
4slan <amalsago@student.42.fr>
2019-12-01 12:11:00 +0100

    removing extra spaces and useless endif

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 09:24:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/16 12:15:04 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/27 15:55:09 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@
 #  define OPEN_MAX 2048
 # endif
 
-#  include <unistd.h>
-#  include <limits.h>
-#  include <string.h>
-#  include "libft.h"
+# include <unistd.h>
+# include <limits.h>
+# include <string.h>
+# include "libft.h"
 
 int		get_next_line(const int fd, char **line);
 
-# endif
 #endif
